d46ab8fd9fa8c0c71d9b01d5a200d4ff58a0106e
Tsvetelina Obretenova <tsvetelinaobretenova@MacBook-Pro-van-Tsvetelina.local>
2020-10-28 21:23:42 +0100

    Add random fonts on load

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-// 1. Grab the head of the page
+const FONTS = [{
+    href: 'https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap',
+    family: "'Luckiest Guy', cursive"
+  },
+  {
+    href: 'https://fonts.googleapis.com/css2?family=Monoton&display=swap',
+    family: "'Monoton', cursive"
+  },
+  {
+    href: 'https://fonts.googleapis.com/css2?family=Shrikhand&display=swap',
+    family: "'Shrikhand', cursive"
+  }
+]
+
+const generateRandomNum = () => {
+  return Math.floor((Math.random() * 3))
+}
+
+const randomNum = generateRandomNum()
+
 const documentHead = document.getElementsByTagName('head')[0]
 
-// 2. create link element
 const linkElement = document.createElement('link')
 linkElement.rel = 'stylesheet'
-linkElement.href = 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;900&display=swap'
+linkElement.href = FONTS[randomNum].href
 
-// 3. add link
+document.body.style.fontFamily = FONTS[randomNum].family
+
 documentHead.appendChild(linkElement)
-
-// Luckiest Guy
-// https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap"
-// font-family: 'Luckiest Guy', cursive;
-//
-// Monoton
-// https://fonts.googleapis.com/css2?family=Monoton&display=swap"
-// font-family: 'Monoton', cursive;
-//
-// Shrikhand
-// https://fonts.googleapis.com/css2?family=Shrikhand&display=swap"
-// font-family: 'Shrikhand', cursive;
